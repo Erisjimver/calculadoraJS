e867c942996a540104bc5517eb61265e8e96ff03
Israel_Jimenez <41273721+Erisjimver@users.noreply.github.com>
2019-06-14 17:43:40 -0500

    funcion igual y resta
    se añadio la funcionalidad de el igual y la resta

--- funciones.js
+++ funciones.js
@@ -1,11 +1,14 @@
 var cifra ="";
 var acumulado=0;
+var sumar=false;
+var restar=0;
 
 function inicio(){
 
 	document.getElementById("display").value=0;
 
 }
+
 function display_numeros(numero){
 
 	//display = document.getElementById("display").value=cifra+numero;
@@ -21,7 +24,43 @@ function suma(){
 	acumulado += parseInt(cifra);
 	cifra = "";
 	document.getElementById("display").value=acumulado;
+	sumar=true;
+
 }
+
+function resta(){
+
+	if(sumar){
+		acumulado = acumulado+parseInt(cifra);
+		document.getElementById("display").value=acumulado;
+	}
+	else
+	{
+		acumulado = acumulado - parseInt(cifra);
+		document.getElementById("display").value=acumulado;
+	}
+		cifra = "";
+		sumar=false;
+		restar=true;
+}
+
+function igual(){
+
+	if(sumar){
+
+		document.getElementById("display").value=acumulado+parseInt(cifra);
+
+	}
+	else if(restar)
+	{
+
+		document.getElementById("display").value=acumulado-parseInt(cifra);
+
+	}
+
+}
+
+
 
 window.addEventListener("load", inicio, false);
 
