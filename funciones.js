f494c37ba4aa40e813a0c0edcedc0795e2e0e8fc
Israel_Jimenez <41273721+Erisjimver@users.noreply.github.com>
2019-06-13 22:36:45 -0500

    Concatenacion de valores
    Se acumula o concatena los numeros en el display, evitando que se borre el numero anterior

--- funciones.js
+++ funciones.js
@@ -1,7 +1,12 @@
+var cifra ="";
 
 function display_numeros(numero){
 
-	display = document.getElementById("display").value=numero;
+	//display = document.getElementById("display").value=cifra+numero;
+
+	document.getElementById("display").value=cifra+numero;
+
+	cifra = document.getElementById("display").value;
 
 }
 
