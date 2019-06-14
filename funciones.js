1bb16669d2b186983af1b3b6ac8e03bb0508b78f
Israel_Jimenez <41273721+Erisjimver@users.noreply.github.com>
2019-06-14 14:11:49 -0500

    funcion suma
    se agrega la funcion suma, ademas de modificar la funcionalidad del display para mostrar resultado y al iniciar muestre el 0.

--- funciones.js
+++ funciones.js
@@ -1,5 +1,11 @@
 var cifra ="";
+var acumulado=0;
 
+function inicio(){
+
+	document.getElementById("display").value=0;
+
+}
 function display_numeros(numero){
 
 	//display = document.getElementById("display").value=cifra+numero;
@@ -9,4 +15,14 @@ function display_numeros(numero){
 	cifra = document.getElementById("display").value;
 
 }
+
+function suma(){
+
+	acumulado += parseInt(cifra);
+	cifra = "";
+	document.getElementById("display").value=acumulado;
+}
+
+window.addEventListener("load", inicio, false);
+
 
